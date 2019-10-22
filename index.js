5b5d5ce67cfb2c07a24deed1fb86eb1528b728ad
Alex Mikhieiev <oleksandr.mikhieiev@just-eat.com>
2019-10-22 13:34:24 +0300

    add middleware

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-var Koa = require('koa');
-var Router = require('koa-router');
-var bodyParser = require('koa-bodyparser');
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
-var app = new Koa();
+const lib =  require('./module');
+
+const app = new Koa();
 app.use(bodyParser());
-var router = new Router();
+const router = new Router();
+
+app.use(async (ctx, next) => {
+    ctx.status = 200
+    console.log(ctx.request.method +"-" + ctx.request.url);
+    next();
+})
 
 router
     .get('/', (ctx, next) => {
-        console.log(ctx)
         ctx.body = 'Hello World';
     })
     .post('/users', async (ctx, next) => {
-        let arr = [];
-        ctx.body = ctx.request.body;
+        ctx.body = lib.greetings(ctx.request.body.name);
     })
 app
     .use(router.routes())
     .use(router.allowedMethods());
+
+
 
 app.listen(3001);
 
